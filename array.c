60d747a2c1b6762548bc5e0b5b39cc2fe354d444
murikihillary285 <murikihillary285@gmail.com>
2024-10-17 20:25:18 +0300

    Add files via upload

---
+++ array.c
@@ -0,0 +1,10 @@
+//arrays in c
+#include <stdio.h>
+int main () {
+    int i;
+   int marks[50]={50,90,60,70,40};
+   for(i=0;i<5;i++){
+   printf("marks[%d]=%d \n",i,marks[i]);
+   }
+return  0;
+}
